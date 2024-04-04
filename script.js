16dac4198d470da8c8de9b885da59e2fb36fc98b
JotaJotaM1 <johnjames_89@hotmail.com>
2024-04-04 11:38:14 -0500

    Virtues section finished and animation for service card adjusted

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ close.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
 
+
+/* Arrows that appear and disappear on service page cards */
+
+document.addEventListener('DOMContentLoaded', () => {
+  let cards = document.querySelectorAll('.card');
+
+  const hideArrow = (arrowIcon) => arrowIcon.classList.add('hide-arrow');
+  const showArrow = (arrowIcon) => arrowIcon.classList.remove('hide-arrow');
+
+  cards.forEach(card => {
+      let arrowIcon = card.querySelector('.arrow-icon');
+
+      card.addEventListener('click', e => {
+          e.stopPropagation();
+          hideArrow(arrowIcon);
+      });
+
+      card.addEventListener('mouseover', () => hideArrow(arrowIcon));
+      card.addEventListener('mouseout', () => showArrow(arrowIcon));
+  });
+
+  document.addEventListener('click', () => {
+      cards.forEach(card => {
+          let arrowIcon = card.querySelector('.arrow-icon');
+          showArrow(arrowIcon);
+      });
+  });
+});
+
